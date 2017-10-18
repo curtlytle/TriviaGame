33887f21f8c330346cbd4a3c699ef6ecdc8ab39a
Curtis Lytle <curtlytle@gmail.com>
2017-10-17 21:47:51 -0600

    More layouting and styling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -15,11 +15,52 @@ $(document).ready(function () {
         displayQuestion("This is a test");
         timerId = setInterval(runTimer, 1000);
         displayTimer(questionTime);
+        displayAnswers("Answers test");
+    });
+
+    $("#question").on("click", function () {
+        clearAnswers();
+        displayAnswers("test");
     });
 
 
     function displayQuestion(question) {
-        $("#question").html("<p>" + question+ "</p>");
+        var $question = $("#question");
+        $question.remove();
+        $question.html("<div><h2>" + question + "</h2></div>");
+    }
+
+    function clearAnswers() {
+        var $div1 = $(".answer1");
+        var $div2 = $(".answer2");
+        var $div3 = $(".answer3");
+        var $div4 = $(".answer4");
+
+        $div1.remove();
+        $div2.remove();
+        $div3.remove();
+        $div4.remove();
+    }
+
+    function displayAnswers(answers) {
+        var $div1 = $("<div>", {class: "answer1"});
+        var $div2 = $("<div>", {class: "answer2"});
+        var $div3 = $("<div>", {class: "answer3"});
+        var $div4 = $("<div>", {class: "answer4"});
+
+        $div1.html("<p>answer 1 hello</p>");
+        $div2.html("<p>answer 2 hello</p>");
+        $div3.html("<p>answer 3 hello</p>");
+        $div4.html("<p>answer 4 hello</p>");
+
+        var answers = $("#answers");
+        answers.show();
+        answers.append($div1);
+        answers.append($div2);
+        answers.append($div3);
+        answers.append($div4);
+
+
     }
 
     function displayTimer(time) {
